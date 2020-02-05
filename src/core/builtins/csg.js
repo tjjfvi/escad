ad20623df0e6b106a64527865477f53615a472f1
tjjfvi <tjjfvi@tjjfvi.com>
2020-02-05 10:44:43 -0500

    added CSG framework, union, add

---
+++ src/core/builtins/csg.js
@@ -0,0 +1,175 @@
+
+const { Work, Product, operators, chainables, Component } = require(".");
+const { Mesh, Face, Vector3 } = require("./Mesh");
+const CSG = require("csg");
+
+class CSGWrapper extends Product {
+
+  static id = "CSGWrapper";
+
+  construct(node){
+    this.node = node;
+  }
+
+  serialize(){
+    function serializePoly(poly){
+      let buf = Buffer.alloc(poly.vertices.length * 6 * 4 + 1);
+      buf.writeUInt8(poly.vertices.length);
+      poly.vertices.map((v, i) => {
+        [v.pos.x, v.pos.y, v.pos.z, v.normal.x, v.normal.y, v.normal.z].map((x, j) =>
+          buf.writeFloatLE(x, (i * 6 + j) * 4 + 1)
+        )
+      })
+      return buf;
+    }
+
+    function serializeNode(node){
+      if(node === null)
+        return Buffer.alloc(0);
+      let ps = node.polygons.map(serializePoly)
+      let l = ps.map(x => x.length).reduce((a, b) => a + b, 0);
+      let front = serializeNode(node.front);
+      let back = serializeNode(node.back);
+      let buf = Buffer.concat([
+        Buffer.alloc(6),
+        ...ps,
+        Buffer.alloc(6),
+        front,
+        Buffer.alloc(6),
+        back
+      ], 18 + l + front.length + back.length);
+      buf.writeUIntLE(l, 0, 6);
+      buf.writeUIntLE(front.length, 6 + l, 6);
+      buf.writeUIntLE(l, 12 + l + front.length, 6);
+      return buf;
+    }
+
+    return serializeNode(this.node);
+  }
+
+  static deserialize(buf){
+    function deserializeNode(buf){
+      if(buf.length === 0)
+        return null;
+      let l = buf.readUIntLE(0);
+      let fl = buf.readUIntLE(6 + l);
+      let bl = buf.readUIntLE(12 + l + fl);
+      let node = new CSG.Node();
+      for(let i = 6; i < 6 + l;) {
+        let pl = buf.readUInt8(i);
+        i++;
+        let I = i;
+        let verts = [];
+        for(; i < I + pl; i += 6 * 4) {
+          let ns = [0, 1, 2, 3, 4, 5].map(x => buf.readFloatLe(i + 4 * x));
+          let vert = new CSG.Vertex(ns.slice(0, 3), ns.slice(3));
+          verts.push(vert);
+        }
+        let poly = new CSG.Polygon(verts);
+        node.polygons.push(poly);
+      }
+      node.front = deserializeNode(buf.subarray(6 + l, 6 + l + fl))
+      node.back = deserializeNode(buf.subarray(12 + l + fl, 12 + l + fl + bl))
+    }
+
+    return new CSGWrapper(deserializeNode(buf));
+  }
+
+}
+
+class MeshToCsgWork extends Work {
+
+  static id="MeshToCsgWork";
+
+  execute([input]){
+    let polygons = input.faces.map(f => new CSG.Polygon(f.points.map(v => new CSG.Vertex(new CSG.Vector(v), []))));
+    let node = new CSG.Node(polygons);
+    return new CSGWrapper(node);
+  }
+
+  transformChildren(children){
+    children = super.transformChildren(children);
+    let [child] = children;
+    if(child instanceof CsgToMeshWork)
+      return this.returnVal = child.children[1];
+    return [child];
+  }
+
+}
+
+class CsgToMeshWork extends Work {
+
+  static id="CsgToMeshWork";
+
+  execute([input]){
+    let faces = [];
+
+    let tv = v => new Vector3(v.pos.x, v.pos.y, v.pos.z);
+    input.node.allPolygons().map(p => {
+      faces.push(...p.vertices.slice(2).map((_, i) =>
+        new Face([tv(p.vertices[0]), tv(p.vertices[i + 1]), tv(p.vertices[i + 2])])
+      ));
+    });
+
+    return new Mesh(faces);
+  }
+
+  transformChildren(children){
+    children = super.transformChildren(children);
+    let [child] = children;
+    if(child instanceof MeshToCsgWork)
+      return this.returnVal = child.children[1];
+    return [child];
+  }
+
+}
+
+class CsgWork extends Work {
+
+  static id = "CsgWork";
+
+  execute(inputs){
+    let nodes = inputs.map(i => i.node.clone());
+    this.args[0].map(arg => {
+      if(arg[0] === "invert")
+        return nodes[arg[1]].invert();
+      if(arg[0] === "clipTo")
+        return nodes[arg[1]].clipTo(nodes[arg[2]]);
+      if(arg[0] === "build")
+        return nodes[arg[1]].build(nodes[arg[2]].allPolygons());
+    })
+    return new CSGWrapper(nodes[this.args[1]]);
+  }
+
+}
+
+Product.Registry.register(CSGWrapper);
+Work.Registry.register(MeshToCsgWork);
+Work.Registry.register(CsgToMeshWork);
+
+let _csg = (args, ops, ret) =>
+  new CsgToMeshWork([new CsgWork(args.map(a => new MeshToCsgWork([a])), ops, ret)])
+let _union = (...args) => {
+  args = args.flat(Infinity);
+  if(args.length === 0)
+    return;
+  if(args.length === 1)
+    return args[0];
+  return _csg(args, args.slice(1).flatMap((_, i) => {
+    let a = 0;
+    let b = i + 1;
+    return [
+      ["clipTo", a, b],
+      ["clipTo", b, a],
+      ["invert", b],
+      ["clipTo", b, a],
+      ["invert", b],
+      ["build", a, b],
+    ];
+  }), 0);
+}
+
+operators.union = (...args) => new Component(_union(...args));
+chainables.add = (comp, ...args) => comp(_union(comp(), ...args));
+
+module.exports = { MeshToCsgWork, CsgToMeshWork, CSGWrapper };
--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 
 module.exports = (escad, p) => {
-  let params = p([
+  /* let params = p([
     { key: "size", type: "number", default: 20 },
     { key: "height", type: "number", default: 10 },
-  ]);
-  console.log(params);
-  return escad.cube(params.size).translate([0, 0, params.height]);
+  ]);*/
+  return escad.cube({s:1,c:[false,false,true]}).add(escad.cube(1))
+  // return escad.cube(params.size).translate([0, 0, params.height]).add(escad.cube(1));
 }
