928d7c10921a4f4fa8f834d24493c6ea5a0df2fa
tjjfvi <tjjfvi@tjjfvi.com>
2020-02-10 18:39:39 -0500

    added sphere

---
+++ src/core/builtins/sphere.js
@@ -0,0 +1,55 @@
+
+const { Work, Component, operators } = require(".");
+const { Mesh, Face, Vector3 } = require("./Mesh");
+const { _diff } = require("./csg");
+
+class SphereWork extends Work {
+
+  static id="SphereWork";
+
+  execute(){
+    let [r, slices, stacks] = this.args;
+
+    let v = (theta, phi) => {
+      theta *= Math.tau / slices;
+      phi *= Math.tau / stacks;
+      phi /= 2;
+      return new Vector3(
+        Math.sin(theta) * Math.sin(phi) * r,
+        Math.cos(theta) * Math.sin(phi) * r,
+        Math.cos(phi) * r,
+      )
+    }
+    return new Mesh([...Array(slices)].flatMap((_, i) =>
+      [...Array(stacks)].flatMap((_, j) => {
+        let vs = [];
+
+        vs.push(v(i, j));
+        if(j > 0)
+          vs.push(v(i + 1, j));
+        if(j < stacks - 1)
+          vs.push(v(i + 1, j + 1));
+        vs.push(v(i, j + 1));
+
+        return [...Array(vs.length - 2)].map((_, i) => new Face([0, i + 1, i + 2].map(x => vs[x])));
+      })
+    ));
+  }
+
+}
+
+operators.hollowSphere = operators.sphere = ({
+  r = 1,
+  slices = 16,
+  stacks = 8,
+  t = r,
+  i = r - t, ir = i,
+  unionDiff = false, ud = unionDiff,
+}) => {
+  let os = new SphereWork([], r, slices, stacks);
+  if(!ir)
+    return new Component(os);
+  let is = new SphereWork([], i, slices, stacks);
+  return ud ? [os, is] : _diff(os, is);
+}
+
--- test/index.js
+++ test/index.js
@@ -6,5 +6,6 @@ module.exports = (escad, p) => {
     x: escad(x).rotate(0, 90, 0),
     y: escad(x).rotate(90, 0, 0),
     z: escad(x),
+    ball: escad.sphere({ r: 2.5, t: .2, ud: true, slices: 64, stacks: 32 }),
   });
 };
