2fd39ed432fcce56c82aaa7e370ed363700cbd7a
tjjfvi <tjjfvi@tjjfvi.com>
2020-02-15 17:24:41 -0500

    tests FractalPyramid, CylCross

---
+++ test/CylCross.js
@@ -0,0 +1,13 @@
+
+module.exports = escad => {
+  let c = escad.udMeld([
+    escad.cyl({ r: 1, i: .8, h: 20, sides: 12, ud: true }),
+    escad.sphere({ r: 2, i: 1.8, ud: true, slices: 24, stacks: 12 }).translate([0, 0, 5]),
+    escad.sphere({ r: 2, i: 1.8, ud: true, slices: 24, stacks: 12 }).translate([0, 0, -5]),
+  ]);
+  return escad.unionDiff({
+    x: escad(c).rotate(0, 90, 0),
+    y: escad(c).rotate(90, 0, 0),
+    z: c,
+  })
+}
