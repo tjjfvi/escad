4a0ed5297541cf9e684cc886fe49f13675dafbbe
tjjfvi <tjjfvi@tjjfvi.com>
2020-02-16 08:05:44 -0500

    added range

---
+++ src/core/builtins/range.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  range: (min, interval,  max) => {
+    if(max == null) {
+      max = interval;
+      interval = 1;
+    }
+    return [...Array(Math.ceil((max - min) / interval))].map((_, i) => i * interval + min);
+  }
+}
