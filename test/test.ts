f39b5aee243d9ea4dff22ffa41a34edab4c1b53f
tjjfvi <tjjfvi@tjjfvi.com>
2020-06-23 17:26:21 -0700

    ...test parameters stuff

--- test/test.ts
+++ test/test.ts
@@ -1,17 +1,25 @@
 
 import escad from "../packages/core";
 import "../packages/builtins/register"
+import { renderFunction } from "../packages/renderer/dist";
+import { NumberParam } from "../packages/parameters/dist";
 
-export default async () => {
+export default renderFunction({
+  outerCubeSize: new NumberParam({ defaultValue: 1 }),
+  innerCubeSize: new NumberParam({ defaultValue: .9 }),
+  sphereSize: new NumberParam({
+    defaultValue: .6,
+    desc: "(diameter)",
+  })
+}, params => {
   const el = (
     escad
-      .cube({ s: 1 })
-      .cube({ s: .9 })
-      .cube({ s: 1, c: false })
+      .cube({ s: params.outerCubeSize })
+      .cube({ s: params.innerCubeSize })
+      .cube({ s: params.outerCubeSize, c: false })
       .diff
-      .sphere({ r: .5, slices: 50, stacks: 25 })
-      .union
+      .sphere({ r: params.sphereSize / 2,  slices: 50, stacks: 25 })
       .meld
   );
   return el;
-};
+});
