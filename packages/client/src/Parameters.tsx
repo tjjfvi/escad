42ee4377c5a44870a8bf299ea34cc7b2bf7946af
tjjfvi <tjjfvi@tjjfvi.com>
2020-06-23 17:25:30 -0700

    ...client parameters stuff

--- packages/client/src/Parameters.tsx
+++ packages/client/src/Parameters.tsx
@@ -10,20 +10,20 @@ export interface Parameter {
 export interface ParameterType {
   id: Id,
   className?: string,
-  component: React.FunctionComponent<{ parameter: Parameter, input: Writeable<Buffer> }>,
+  component: React.FunctionComponent<{ parameter: Parameter, input: Writeable<Buffer | null> }>,
 }
 
 export interface ParameterTypeInput {
   id: Id | Promise<Id>,
   className?: string,
-  component: React.FunctionComponent<{ parameter: Parameter, input: Writeable<Buffer> }>,
+  component: React.FunctionComponent<{ parameter: Parameter, input: Writeable<Buffer | null> }>,
 }
 
 // eslint-disable-next-line func-call-spacing
 const parameterTypeResolveMap = new Map<Id, (value: ParameterType) => void>();
 const parameterTypeMap = new Map<Id, Promise<ParameterType>>();
 
-export const registerParameterType = async <V extends any>(type: ParameterTypeInput) => {
+export const registerParameterType = async (type: ParameterTypeInput) => {
   const id = await type.id;
   if(!parameterTypeMap.has(id))
     parameterTypeMap.set(id, Promise.resolve({ ...type, id }));
@@ -43,7 +43,7 @@ export const getParameterType = (id: Id) => {
   return promise;
 }
 
-export const Parameter = ({ parameter, input }: { parameter: Parameter, input: Observable<Buffer> }) => {
+export const Parameter = ({ parameter, input }: { parameter: Parameter, input: Observable<Buffer | null> }) => {
   const parameterType = useFromProm(() => getParameterType(parameter.type)).use()();
   if(!parameterType)
     return <div className="Parameter none"><IdComp id={parameter.type}/></div>;
