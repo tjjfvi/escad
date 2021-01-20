c9ee5fdbb74ca39afdc35344f2e2aa4a31707569
tjjfvi <tjjfvi@tjjfvi.com>
2021-01-19 18:35:14 -0800

    remove old code in refactorTest

--- test/refactorTest.ts
+++ test/refactorTest.ts
@@ -1,35 +1,6 @@
 
 import "../packages/csg/register";
 import { cube } from "../packages/solids/dist";
-// import { Mesh, Vector3 } from "../packages/mesh/dist";
-// import { writeFile } from "fs-extra";
-
-// import { artifactManager, conversionRegistry, FsArtifactStore } from "../packages/core/dist";
-
-// const stlVector = (v: Vector3) => `${v.x} ${v.y} ${v.z}`
-// const stl = (mesh: Mesh) =>
-//   "solid test\n" +
-//   (mesh.faces.flatMap(face => [
-//     `  facet normal ${stlVector(face.plane.normal)}`,
-//     `    outer loop`,
-//     `      vertex ${stlVector(face.points[0])}`,
-//     `      vertex ${stlVector(face.points[1])}`,
-//     `      vertex ${stlVector(face.points[2])}`,
-//     `    endloop`,
-//     `  endfacet`,
-//   ]).join("\n")) +
-//   "\nendsolid test";
-
-// (async () => {
-//   artifactManager.artifactStores.unshift(new FsArtifactStore(__dirname + "/../artifacts"));
-//   // const x = Mesh.convert(model().toArrayFlat()[0]);
-//   const x = await artifactManager.lookupRef([
-//     conversionRegistry.artifactStoreId,
-//     Mesh.id,
-//     model().toArrayFlat()[0],
-//   ]) as Mesh
-//   await writeFile(__dirname + "/test.stl", stl(x), "utf8");
-// })()
 
 export default () => (
   cube({ s: 1 })
