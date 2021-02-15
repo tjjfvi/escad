95b00ebc1dddc19ba242f14180b6420c8bed5411
tjjfvi <tjjfvi@tjjfvi.com>
2021-02-15 13:44:25 -0800

    make Pane resizable

--- packages/client/src/Pane.tsx
+++ packages/client/src/Pane.tsx
@@ -8,14 +8,58 @@ export type PaneArgs = {
   children: React.ReactNode,
   left?: boolean,
   right?: boolean,
+  defaultWidth?: number,
+  resizable?: boolean,
+  defaultOpen?: boolean,
+  minWidth?: number,
 }
 
 // eslint-disable-next-line react/prop-types
-const Pane = ({ name, className = name, children, right = false, left = !right }: PaneArgs) => {
-  const open = useObservable.use(false);
-  return <div className={(open() ? "open " : "") + (left ? "left" : "right") + " Pane " + className}>
+const Pane = ({
+  name,
+  className = name,
+  children,
+  right = false,
+  left = !right,
+  defaultWidth = 500,
+  resizable = true,
+  defaultOpen = false,
+  minWidth = 100,
+}: PaneArgs) => {
+  const width = useObservable.use(defaultWidth);
+  const open = useObservable.use(defaultOpen);
+  const resizing = useObservable.use(false);
+  if(!resizing() && width() < minWidth)
+    width(minWidth);
+  return <div
+    className={
+      "Pane " +
+      (left ? "left " : "right ") +
+      (open() ? "open " : "") +
+      (resizing() ? "resizing " : "") +
+      (resizable && open() ? "resizable " : "") +
+      className
+    }
+    style={{ maxWidth: open() ? Math.max(width(), minWidth) : 50 }}
+  >
+    <div
+      className="border"
+      onMouseDown={() => {
+        if(!resizable || !open()) return;
+        resizing(true);
+        const mouseMoveHandler = (e: MouseEvent) => {
+          if(e.buttons)
+            width(width() + e.movementX * (left ? 1 : -1));
+          else {
+            document.documentElement.removeEventListener("mousemove", mouseMoveHandler)
+            resizing(false);
+          }
+        }
+        document.documentElement.addEventListener("mousemove", mouseMoveHandler);
+      }}
+    />
     <div className="side" onClick={() => open(!open())}><span>{name}</span></div>
-    <div>{children}</div>
+    <div className="content" style={{ minWidth: Math.max(width(), minWidth) - 50 }}>{children}</div>
   </div>
 }
 
