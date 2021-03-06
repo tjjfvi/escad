4cbf68a8d439f2d477fe040eb31aa678fc8ca9b3
tjjfvi <tjjfvi@tjjfvi.com>
2021-03-06 10:04:11 -0800

    remove unused Parameter.rename, add Parameter.isParameter

--- packages/core/src/Parameter.ts
+++ packages/core/src/Parameter.ts
@@ -1,15 +1,13 @@
+import { checkTypeProperty } from "./checkTypeProperty";
 import { ScopedId } from "./Id";
 
 export interface Parameter<V> {
-  readonly type: ScopedId<string>,
+  readonly type: ScopedId<"Parameter">,
   readonly defaultValue: V,
   readonly name?: string,
   readonly desc?: string,
 }
 
 export const Parameter = {
-  rename: <V, T extends Parameter<V>>(parameter: T, name: string): T => ({
-    ...parameter,
-    name,
-  })
+  isParameter: checkTypeProperty.idScope<Parameter<unknown>>("Parameter")
 }
