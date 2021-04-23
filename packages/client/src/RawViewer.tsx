110a24803e8e87f53240e115ccaa441e3c308732
tjjfvi <tjjfvi@tjjfvi.com>
2021-04-23 15:44:32 -0700

    use loading icon in RawViewer

--- packages/client/src/RawViewer.tsx
+++ packages/client/src/RawViewer.tsx
@@ -1,11 +1,12 @@
 
 import "../stylus/RawViewer.styl"
-import { Hierarchy, NameHierarchy } from "@escad/core"
+import { Hierarchy } from "@escad/core"
 import { UnknownProduct, UnknownProductType } from "@escad/core"
 import React from "react"
 import { observer, useFromPromise } from "rhobo"
 import { HierarchyView } from "./HierarchyView"
 import { viewerRegistry } from "./ViewerRegistry"
+import { Loading } from "./Loading"
 
 viewerRegistry.register<UnknownProduct, { product: UnknownProduct }>({
   type: UnknownProductType.create(),
@@ -15,7 +16,9 @@ viewerRegistry.register<UnknownProduct, { product: UnknownProduct }>({
     className: "RawViewer",
     component: observer(({ inputs }) => {
       const hierarchyPromise = Promise.all(inputs).then(x => Hierarchy.from(x.map(x => x.product.product), true))
-      const hierarchy = useFromPromise(hierarchyPromise, NameHierarchy.create({ name: "Loading" }))()
+      const hierarchy = useFromPromise(hierarchyPromise)()
+      if(!hierarchy)
+        return <Loading/>
       return <div className="inner"><HierarchyView hierarchy={hierarchy} selectable={false}/></div>
     }),
     weight: 0,
