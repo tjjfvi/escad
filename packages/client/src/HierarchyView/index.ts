272501119414d473dd64c2088b96b8ce5cf21a1a
tjjfvi <tjjfvi@tjjfvi.com>
2021-04-25 17:59:47 -0700

    fix index re HierarchyView

---
+++ packages/client/src/HierarchyView/index.ts
@@ -0,0 +1,14 @@
+
+// @create-index {"mode":"*"}
+
+export * from "./HierarchyView"
+export * from "./NestableSpan"
+export * from "./State"
+export * from "./Tree"
+export * from "./TreeText"
+export * from "./TreeTextView"
+export * from "./TreeView"
+export * from "./finalizeTree"
+export * from "./flattenTree"
+export * from "./hierarchyToTree"
+
