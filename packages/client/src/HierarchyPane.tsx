ad7f9b45b215e43bbef81c5720da0525f80bcb03
tjjfvi <tjjfvi@tjjfvi.com>
2021-05-04 19:19:27 -0700

    fix HierarchyPane imports

--- packages/client/src/HierarchyPane.tsx
+++ packages/client/src/HierarchyPane.tsx
@@ -3,11 +3,9 @@ import "../stylus/Hierarchy.styl"
 import React, { useContext } from "react"
 import { observer, useObservable } from "rhobo"
 import { ClientState } from "./ClientState"
-import { HierarchyToTreeEngine, HierarchyView } from "./HierarchyView"
+import { HierarchyToTreeEngine, HierarchyView, httDefaultEngine, httSimpleEngine } from "./HierarchyView"
 import { Pane } from "./Pane"
-import { httDefaultEngine } from "./HierarchyView/httDefaultEngine"
 import { Dropdown } from "./Dropdown"
-import { httSimpleEngine } from "./HierarchyView/httSimpleEngine"
 
 export const HierarchyPane = observer(() => {
   const engine = useObservable<HierarchyToTreeEngine>(httDefaultEngine)
