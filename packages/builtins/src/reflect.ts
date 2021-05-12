d2e9f3539455596246f883764442e7a08b9c9ebe
tjjfvi <tjjfvi@tjjfvi.com>
2021-05-12 09:24:18 -0700

    add reflect, reflect[XYZ]

---
+++ packages/builtins/src/reflect.ts
@@ -0,0 +1,23 @@
+import { Component, ConvertibleTo, mapOperation, TupleProduct } from "@escad/core"
+import { Mesh, Transform } from "."
+import { FlipFaces } from "./flip"
+import { Matrix4 } from "./Matrix4"
+
+export const reflect = Component.create("reflect", (axis: "x" | "y" | "z", center = 0) => {
+  const scale = { x: 1, y: 1, z: 1, [axis]: -1 }
+  const translate = { x: 0, y: 0, z: 0, [axis]: center }
+  const matrix = Matrix4.multiply(
+    Matrix4.multiply(
+      Matrix4.translate(translate.x, translate.y, translate.z),
+      Matrix4.scale(scale.x, scale.y, scale.z),
+    ),
+    Matrix4.translate(-translate.x, -translate.y, translate.z),
+  )
+  return mapOperation("reflect", (leaf: ConvertibleTo<Mesh>) =>
+    FlipFaces.create(Transform.create(TupleProduct.create([matrix, leaf] as const))),
+  )
+})
+
+export const reflectX = Component.create("reflectX", (center = 0) => reflect("x", center))
+export const reflectY = Component.create("reflectY", (center = 0) => reflect("y", center))
+export const reflectZ = Component.create("reflectZ", (center = 0) => reflect("z", center))
