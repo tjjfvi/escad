e7fbf962240decced874d8df212e9774d9e0a613
vjjft <vjjft@vjjft.com>
2021-05-14 21:05:17 -0700

    extract loadFile, call on server init

--- packages/protocol/src/serverClient.ts
+++ packages/protocol/src/serverClient.ts
@@ -1,18 +1,18 @@
 
 import { Messenger } from "@escad/messages"
 import { Hash, Log } from "@escad/core"
-import { Info } from "./serverRenderer"
+import { RenderInfo } from "./serverRenderer"
 
 export type ServerClientShape = {
   lookupRef(loc: readonly unknown[]): Promise<Hash<unknown>>,
-  run(params?: unknown): Promise<Info>,
+  run(params?: unknown): Promise<RenderInfo>,
 }
 
 export type ClientServerShape = {}
 
 export type ServerClientEvents = {
   changeObserved: [],
-  info: [info: Info],
+  info: [info: RenderInfo],
   bundleStart: [],
   bundleFinish: [hash: Hash<unknown>],
   log: [log: Hash<Log> | null],
