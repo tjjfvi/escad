a646f936d43eb7558f2af32a343ba616c69950f1
tjjfvi <tjjfvi@tjjfvi.com>
2021-05-16 15:58:12 -0700

    update examples/ChainFabric

--- examples/ChainFabric.ts
+++ examples/ChainFabric.ts
@@ -15,8 +15,6 @@ function range(...args: [number] | [number, number] | [number, number, number]):
 }
 
 export default () => {
-  // console.log(global.timers);
-
   type ConnectorStyle = "all" | "none" | "edge"
 
   const
@@ -25,44 +23,40 @@ export default () => {
     thickness = .9,
     connectorHeight = .6,
     // barAngle = 20,
-    width = 100,
-    height = 100,
+    width = 200,
+    height = 200,
     translateMultiplier = 1.1,
     connectorHoleMultiplier = .25,
     connectorStyle = "edge" as ConnectorStyle
 
-  const subpiece = () =>
+  const subpiece =
     escad
-      .cube({ dimensions: [Math.sqrt(2) * or, or - ir, thickness], center: true })
+      .cube({ size: [Math.sqrt(2) * or, or - ir, thickness] })
       .translate([0, ir / 2 - or / 2, 0])
       .rotateZ(45)
       .translate([or / 2, or / 2, 0])
 
-  const subpieceConnector = () =>
+  const subpieceConnector =
     escad
-      .cube({ dimensions: [or - ir, or - ir, connectorHeight], center: true })
+      .cube({ size: [or - ir, or - ir, connectorHeight] })
       .rotateZ(-45)
       .translate([or - Math.sqrt(2) / 2 * (or - ir), 0, 0])
 
-  const piece = () => escad({
-    piece: escad.union([0, 90, 180, 270].map(angle =>
-      escad.union(
-        subpieceConnector(),
-        subpiece().translate([0, 0, (angle % 180 ? 1 : -1) * (connectorHeight + thickness) / 2]),
-      ).rotateZ(-angle),
-    )),
-  })
+  const piece = escad.union([0, 90, 180, 270].map(angle =>
+    escad.union(
+      subpieceConnector(),
+      subpiece().translate([0, 0, (angle % 180 ? 1 : -1) * (connectorHeight + thickness) / 2]),
+    ).rotateZ(-angle),
+  ))
 
-  const connector = () => ({
-    connector: escad.diff(
-      piece(),
-      escad
-        .cube({ d: [or, thickness * connectorHoleMultiplier, thickness * 2 + connectorHeight / 2], c: true })
-        .rotate(30, 0, 0)
-        .translate([-or / 2, 0, -thickness / 2 - connectorHeight])
-        .rotateZ(-45),
-    ),
-  })
+  const connector = escad.diff(
+    piece(),
+    escad
+      .cube({ size: [or, thickness * connectorHoleMultiplier, thickness * 2 + connectorHeight / 2] })
+      .rotate(30, 0, 0)
+      .translate([-or / 2, 0, -thickness / 2 - connectorHeight])
+      .rotateZ(-45),
+  )
 
   let translateAmount = translateMultiplier * or
 
@@ -70,19 +64,19 @@ export default () => {
 
   return escad
     .translate(tf(width), tf(height), 0)(
-      escad.meld(range(0, translateAmount, width - or * 2).map(x =>
-        escad.meld(range(0, translateAmount, height - or * 2).map(y =>
-          escad.tY(y + or)((
+      range(0, translateAmount, width - or * 2).map(x =>
+        range(0, translateAmount, height - or * 2).map(y =>
+          escad.translate(x + or, y + or, 0)(
             connectorStyle === "none"
               ? piece
               : connectorStyle === "all"
                 ? connector
                 : x === 0 || y === 0
                   ? connector
-                  : piece
-          )()),
-        )).tX(x + or),
-      )),
-    )
+                  : piece,
+          ),
+        ),
+      ),
+    ).meld()
 
 }
