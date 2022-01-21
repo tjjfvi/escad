3e1b5b0c8b98e7473184a166f114d4b9c1c581d6
tjjfvi <tjjfvi@tjjfvi.com>
2022-01-20 20:31:31 -0500

    remove old ts-ignore

--- examples/Logo.ts
+++ examples/Logo.ts
@@ -24,7 +24,6 @@ export default renderFunction(
     const main = escad.cube({ size: outerSize })
     const inner = escad.cube({ size: innerSize })
     const corner = escad.cube({ size: outerSize, shift: 1 })
-    // @ts-ignore
     const final = main
       ._(hollow ? escad.sub(inner) : escad)
       .sub(corner)
